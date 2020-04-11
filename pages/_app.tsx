ff8d2bf1d57178e162a0159ea9866cd396980abe
Talha Balaj <talhabalaj@hotmail.com>
2020-04-11 10:05:39 +0500

    google tag manager added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,18 @@ import App from 'next/app'
 import NavBar from '../components/NavBar';
 import styles from './_app.scss'
 import Head from '../components/Head';
+import TagManager, { TagManagerArgs } from 'react-gtm-module'
+
+const tagManagerArgs: TagManagerArgs = {
+    gtmId: 'GTM-NTDGTW7'
+}
 
 export default class myApp extends App {
+
+    componentDidMount() { 
+        TagManager.initialize(tagManagerArgs)
+    }
+
     render() {
         const { Component, pageProps } = this.props;
         return (
