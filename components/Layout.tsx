32adfa8e0d66d37a941d05a8f890d8fc85e38dce
Talha Balaj <talhabalaj@hotmail.com>
2020-04-18 16:35:13 +0500

    clean up

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,6 @@
 import { ThemeProvider } from "styled-components";
 import { theme, GlobalStyles } from "../config/theme";
 
-
 export const Layout = ({ children }) => (
     <ThemeProvider theme={theme}>
         <GlobalStyles />
