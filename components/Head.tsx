d9771b860005c6541742986f5f7aa87a733861ee
Talha Balaj <talhabalaj@hotmail.com>
2020-04-19 08:32:07 +0500

    added favicon

--- components/Head.tsx
+++ components/Head.tsx
@@ -6,6 +6,9 @@ export default () => (
         <meta name="Description" content="I am Talha Balaj, a Full-Stack JavaScript Developer, doing Bachelors of Computer Science and experimenting with code in free time.  " />
         <meta name="Keywords" content="code, developer, full-stack, html, css, nodejs, js, es6, react, typescript " />
         <link rel="stylesheet" href="/css/iconmonstr-iconic-font.min.css" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800;900&display=swap" rel="stylesheet" />
     </Head>
 )
